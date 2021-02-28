11cf45e245d65cd5c7dd38227cb960ae35e86ac6
Hamza Yousuf <hamza.yousuf@treasurycube.com>
2021-02-28 12:40:36 +0500

    add firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,1 +1,16 @@
-/// firebase setup
+//firebase setup
+import firebase from 'firebase';
+import "firebase/auth";
+import "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAK98-Z-JcoidAyjvjlaI4qvTobL5V06os",
+    authDomain: "hackathon-8fa65.firebaseapp.com",
+    projectId: "hackathon-8fa65",
+    storageBucket: "hackathon-8fa65.appspot.com",
+    messagingSenderId: "468134804825",
+    appId: "1:468134804825:web:a6ad2224cbbc425de0e048"
+};
+
+firebase.initializeApp(firebaseConfig);
