139eb3369c893ecc4b8ff8bad02af0fd5ed4954a
aliraza0001 <aliarain214@gmail.com>
2021-02-28 12:31:38 +0500

    created compoanets and service folder

---
+++ src/components/companyForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const companyForm = () => {
+  return (
+    <View>
+      <Text>company form</Text>
+    </View>
+  );
+};
+
+export default companyForm;
---
+++ src/components/studentForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const studentForm = () => {
+    return (
+        <View>
+            <Text>student Form</Text>
+        </View>
+    )
+}
+
+export default studentForm
